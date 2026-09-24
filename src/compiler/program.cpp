6bc47bd835e9d2ab999537fa112459dfc19f7f79
Vikmanou <68619882+Vikmanou@users.noreply.github.com>
2026-09-24 08:38:31 -0400

    Construct progam instructions

--- src/compiler/program.cpp
+++ src/compiler/program.cpp
@@ -1,9 +1,32 @@
 #include "chad/compiler/program.h"
 
+#include <optional>
+
+#include "chad/core/error.h"
+#include "chad/core/text.h"
+#include "chad/compiler/instruction.h"
+#include "chad/compiler/lexer.h"
+
 namespace chad {
+
+namespace {
+
+Instruction fromToken(const Token& token) {
+    Instruction instruction;
+    instruction.line = token.line;
+
+    
+    return instruction;
+}
+
+}
 
 Program compile(const std::string& source) {
     Program program;
+
+    for (const Token& token : lex(source)) {
+        program.instructions.push_back(fromToken(token));
+    }
 
     return program;
 }
