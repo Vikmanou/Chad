44032eb1130803ad31e082659bd8d81c1679e9ca
Vikmanou <68619882+Vikmanou@users.noreply.github.com>
2026-09-25 23:46:53 -0400

    Engine skeleton

---
+++ include/chad/net/engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "chad/lang/program.h"
+
+namespace chad {
+
+void run(const Program& program);
+
+}
---
+++ src/net/engine.cpp
@@ -0,0 +1,10 @@
+#include "chad/net/engine.h"
+
+#include "chad/lang/program.h"
+
+namespace chad {
+
+void run(const Program& program) {
+}
+
+}
